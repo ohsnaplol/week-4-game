a23b9dc60a686fd4f0e181ecea0eefeb5c2ebe29
ohsnaplol <emyb@bellsouth.net>
2017-11-15 11:09:04 -0500

    added some logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,38 @@
 $(document).onReady(function() {
+  var currentHero;
+  var playerIsSelectingHero = true;
 
+  $("#warrior").on("click", function() {
+    if playerIsSelectingHero {
+      currentHero = "wa";
+    }
+    playerIsSelectingHero = false;
+  });
+  $("#wizard").on("click", function() {
+    if playerIsSelectingHero {
+      currentHero = "wi";
+    }
+    playerIsSelectingHero = false;
+  });
+  $("#rogue").on("click", function() {
+    if playerIsSelectingHero {
+      currentHero = "ro";
+    }
+    playerIsSelectingHero = false;
+  });
+  $("#priest").on("click", function() {
+    if playerIsSelectingHero {
+      currentHero = "pr";
+    }
+    playerIsSelectingHero = false;
+  });
+
+  function startGame(hero) {
+    $("#currentCharacter").innerHTML("");
+  }
 })
+
+var hero {
+  hp,
+  atk;
+}
