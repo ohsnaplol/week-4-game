0931412f2a81a7a5426e3f70c8e7d71d8a17347e
ohsnaplol <emyb@bellsouth.net>
2017-11-15 19:00:41 -0500

    functional gameplay

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
   var playerCanChooseHero = true;
   var playerCanChooseEnemy = true;
-  var playerCanAttack = false;
+  var playerCanatk = false;
   var currentHero;
   var currentDefender;
 
   $("#enemywarrior,#enemywizard,#enemyrogue,#enemypriest").hide();
   $("#playerwarrior,#playerwizard,#playerrogue,#playerpriest").hide();
   $("#defendingwarrior,#defendingwizard,#defendingrogue,#defendingpriest").hide();
+  $("#currentCharacterLabel,#atkButton,#currentDefenderLabel,#enemiesLabel").hide();
+  $("#attackButton").hide();
 
+  // Player picks the hero they want to be
   $("#warrior").on("click", function() {
     if (playerCanChooseHero) {
       setupGame("wa");
@@ -34,26 +37,14 @@ $(document).ready(function() {
     }
   });
 
-  $("#enemywarrior").on("click", function() {
-    currentDefender = characters.Warrior;
-    console.log(currentDefender.health);
-  });
-  $("#enemywizard").on("click", function() {
-    currentDefender = characters.Wizard;
-    console.log(currentDefender.health);
-  });
-  $("#enemyrogue").on("click", function() {
-    currentDefender = characters.Rogue;
-    console.log(currentDefender.health);
-  });
-  $("#enemypriest").on("click", function() {
-    currentDefender = characters.Priest;
-    console.log(currentDefender.health);
-  });
-
+  // Once a hero is chosen, move player and and enemies into position
+  // Set currentHero variable to what player chose
+  // Show enemies
   function setupGame(hero) {
     $("#warrior,#wizard,#rogue,#priest").hide();
-    // Move current character and enemies into position
+    $("#chooseCharacterLabel").hide();
+    $("#currentCharacterLabel,#enemiesLabel").show();
+
     if(hero === "wa") {
       currentHero = characters.Warrior;
       $("#playerwarrior").show();
@@ -66,37 +57,99 @@ $(document).ready(function() {
       currentHero = characters.Rogue;
       $("#playerrogue").show();
       $("#enemywarrior,#enemywizard,#enemypriest").show();
-    } else if (hero === "pri") {
+    } else if (hero === "pr") {
       currentHero = characters.Priest;
       $("#playerpriest").show();
       $("#enemywarrior,#enemywizard,#enemyrogue").show();
     }
   }
 
-  $("#attackButton").on("click", function() {
+  // Select defender for player to atk, remove from enemy list
+  $("#enemywarrior").on("click", function() {
+    if (playerCanChooseEnemy) {
+      currentDefender = characters.Warrior;
+      $("#enemywarrior").hide();
+      $("#defendingwarrior,#currentDefenderLabel,#attackButton").show();
+      playerCanChooseEnemy = false;
+    }
+  });
+  $("#enemywizard").on("click", function() {
+    if (playerCanChooseEnemy) {
+      currentDefender = characters.Wizard;
+      $("#enemywizard").hide();
+      $("#defendingwizard,#currentDefenderLabel,#attackButton").show();
+      playerCanChooseEnemy = false;
+    }
+  });
+  $("#enemyrogue").on("click", function() {
+    if (playerCanChooseEnemy) {
+      currentDefender = characters.Rogue;
+      $("#enemyrogue").hide();
+      $("#defendingrogue,#currentDefenderLabel,#attackButton").show();
+      playerCanChooseEnemy = false;
+    }
+  });
+  $("#enemypriest").on("click", function() {
+    if (playerCanChooseEnemy) {
+      currentDefender = characters.Priest;
+      $("#enemypriest").hide();
+      $("#defendingpriest,#currentDefenderLabel,#attackButton").show();
+      playerCanChooseEnemy = false;
+    }
+  });
 
+  $("#attackButton").on("click", function() {
+    // if player has health and enemy has health
+    if (currentHero.hp > 0 && currentDefender.hp > 0) {
+      // Player hits the bad guy and is buffed
+      currentDefender.hp -= currentHero.atk;
+      currentHero.atk += currentHero.atk;
+      // if bad guy is still alive
+      if (currentDefender.hp > 0) {
+        if(currentDefender.id === "pr") {
+          currentDefender.hp += 10;
+          console.log("The priest healed himself for 10hp!");
+        }
+        // Bad guy hits player back
+        currentHero.hp -= currentDefender.enemyatkBack;
+        if (currentHero.hp <= 0) {
+          console.log("You died");
+        }
+      } else {
+        playerCanChooseEnemy = true;
+        $("#defendingwarrior,#defendingwizard,#defendingrogue,#defendingpriest,#attackButton").hide();
+        console.log("You defeated enemy");
+      }
+      console.log("Your hero has " + currentHero.hp);
+      console.log("Bad guy has " + currentDefender.hp);
+    }
   })
+
 })
 
 var characters = {
    Warrior: {
-     health: 200,
-     attack: 9,
-     enemyAttackBack: 15
+     id: "wa",
+     hp: 200,
+     atk: 9,
+     enemyatkBack: 9
    },
    Wizard: {
-     health: 150,
-     attack: 12,
-     enemyAttackBack: 15
+     id: "wi",
+     hp: 150,
+     atk: 12,
+     enemyatkBack: 12
    },
    Rogue: {
-     health: 100,
-     attack: 20,
-     enemyAttackBack: 15
+     id: "ro",
+     hp: 100,
+     atk: 20,
+     enemyatkBack: 20
    },
    Priest: {
-     health: 300,
-     attack: 5,
-     enemyAttackBack: 15
+     id: "pr",
+     hp: 100,
+     atk: 5,
+     enemyatkBack: 5
    }
  }
